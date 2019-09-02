29edc881b51b0db09e0143ca914bd6fcfe38933c
Felix <felix.urtubia@gmail.com>
2019-09-02 13:53:33 -0400

    update test for DeckModel

--- app/deck_app/tests.py
+++ app/deck_app/tests.py
@@ -8,11 +8,11 @@ class DeckTestCase(TestCase):
         userDeck = UserDeck.objects.create()
         userDeck.id_owner = 1
         userDeck.stats_id = 1
-        userDeck.mainDeck = Deck.objects.create(name="Mazo de prueba, main deck 1")
-        userDeck.sideDeck = Deck.objects.create(name="Mazo de prueba, side deck 1")
-        userDeck.extraDeck = Deck.objects.create(name="Mazo de prueba, extra deck 1")
+        userDeck.mainDeck = Deck.objects.create(cardsMin=0, cardsMax=15, cardsCount=0)
+        userDeck.sideDeck = Deck.objects.create(cardsMin=0, cardsMax=15, cardsCount=0)
+        userDeck.extraDeck = Deck.objects.create(cardsMin=0, cardsMax=15, cardsCount=0)
 
     def test_deck_can_be_created_and_retrieved(self):
         deck_1 = UserDeck.objects.filter(id_owner=1)[0]
-        self.assertEqual(deck_1.mainDeck, "Mazo de prueba, main deck 1")
-        self.assertEqual(deck_1.mainDeck(), "Mazo de prueba, side deck 1")
+        self.assertEqual(deck_1.mainDeck, "This deck has 0 cards")
+        self.assertEqual(deck_1.mainDeck(), "This deck has 0 cards")
